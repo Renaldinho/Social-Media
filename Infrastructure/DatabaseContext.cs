fbed8278b91d9f17acdeb18802ab8740ee9218a4
Renaldinho <rena0968@easv365.dk>
2024-02-24 23:24:44 +0100

    .

---
+++ Infrastructure/DatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure;
+
+public class DatabaseContext: DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.Property(e => e.Email).IsRequired();
+        });
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,8 @@
+﻿using Infrastructure.Interfaces;
+
+namespace Infrastructure.Repositories;
+
+public class UserRepository: IUserRepository
+{
+    
+}
